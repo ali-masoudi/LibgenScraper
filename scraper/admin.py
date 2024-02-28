e93f134c56125bdb486cd1e664133f306260abcd
Ali Masoudi <ali.masoudi@live.com>
2024-02-28 13:47:21 +0330

    Models registered.

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,1 +1,24 @@
+from django.contrib import admin
+
+from .models import Keyword, Book, SearchResult
+
+
 # Register your models here.
+
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ('keyword', 'creation_date',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'authors', 'volume', 'series', 'publisher', 'city', 'year', 'is_downloaded')
+    list_filter = ('is_downloaded', 'creation_date', 'modification_date')
+    search_fields = ('title', 'authors', 'publisher', 'year')
+
+
+@admin.register(SearchResult)
+class SearchResultAdmin(admin.ModelAdmin):
+    list_display = ('keyword', 'book', 'book_id', 'creation_date',)
+    list_filter = ('keyword', 'creation_date',)
+    search_fields = ('book__title', 'keyword__keyword')
